efb67fa359558581c195f1e5b91a2e7fc7a0051f
Exant64 <warpcauldronproject@gmail.com>
2024-06-07 18:27:54 +0200

    catch up to sa1dc and sa2dc versions

--- dcsplit.py
+++ dcsplit.py
@@ -21,6 +21,7 @@ with open(dcsplit_config['target_path'], 'rb') as file:
 
 labelled_addresses = set()
 label_names = {}
+need_export = set()
 
 def add_label(addr, name):
     labelled_addresses.add(addr)
@@ -38,7 +39,7 @@ def label_candidate(address):
     return False
 
 # this handles the sections between functions that hold the data that the functions were using
-def add_label_funcData(addr, short):
+def add_label_funcData(seg_start, seg_end, addr, short):
     labelled_addresses.add(addr)
 
     # if a short, add to list of the 2 byte labels (data.w)
@@ -52,6 +53,8 @@ def add_label_funcData(addr, short):
     # check if it happens to be a pointer to something valid in the binary
     integer = struct.unpack("I", filecontent[addr - key : addr - key + 4])[0]
     if label_candidate(integer) == True:
+        if integer < seg_start or integer >= seg_end:
+            need_export.add(integer)
         labelled_addresses.add(integer)
 
 data_short_addresses = set()
@@ -60,9 +63,11 @@ labelled_bss_addresses = set()
 
 if 'blacklist_code' in dcsplit_config:
     for elem in dcsplit_config['blacklist_code']:
-        add_label_funcData(elem, True)
+        # hack, we dont know the segstart or segend, so 0,0 does for now
+        add_label_funcData(0, 0, elem, True)
 
 key = dcsplit_config['key'] 
+big_endian_nop_padding = 'big_endian_nop_padding' in dcsplit_config
 
 if 'symbol_addrs_path' in dcsplit_config:
     with open(dcsplit_config['symbol_addrs_path'], 'r') as symbols_file:
@@ -81,6 +86,7 @@ def check_invalid_data_address(address):
     return (address - 2) in labelled_addresses and (address - 2) in data_long_addresses
 
 add_label(key, dcsplit_config['entry'])
+need_export.add(key)
 
 def addr_to_label(addr):
     return "#" + addr_to_label_noh(addr)
@@ -101,8 +107,16 @@ def get_instruction_at(address, offset):
     code = get_short_at(offset)
     return sh4.decode(code[0], address)
 
+def is_nop_padding(address, offset):
+    code = get_short_at(offset)
+    # check if yaml is in "weird big endian nop" mode
+    if big_endian_nop_padding:
+        return code[0] == 0x0900
+    else:
+        return sh4.decode(code[0], address).op == sh4.OP.NOP
+
 # Calls callback for every instruction in the specified code section
-def disasm_iter(offset, address, size, callback):
+def disasm_iter(seg_start, seg_end, offset, address, size, callback):
     if size == 0:
         return
     start = address
@@ -111,14 +125,10 @@ def disasm_iter(offset, address, size, callback):
         
         code = struct.unpack("H", filecontent[(offset + (address-start)) : (offset + (address-start)) + 2])
         insn = sh4.decode(code[0], address)
-        #print(sh4.disasm(code[0], address))
+        
         if insn is not None:
-            callback(start, end, address, offset + address - start, insn, code[0])
+            callback(seg_start, seg_end, address, offset + address - start, insn, code[0])
         address += 2
-        #if address < end:
-        #    o = offset + address - start
-        #    callback(address, offset + address - start, None, filecontent[o : o + 4])
-        #    address += 4
 
 # helps converting the operands of an instruction to valid asm
 def oper2str(insn, oper):
@@ -179,8 +189,6 @@ def disasm(arg, pc = 0):
         decoded = decode(arg, pc)
     else:
         decoded = arg
-
-    #print(decoded)
 
     return decoded2str(decoded)
 
@@ -259,20 +267,31 @@ def bss_iter(offset, address, size):
     if size == 0:
         return
 
-    #sorted_bss_labels = filter(lambda x: x - key > len(filecontent), list(sorted(labelled_addresses)))
-    #print(sorted_bss_labels)
-    start = address
-    end = address + size
-    while address < end:
-        if address in labelled_addresses:
-            if address in label_names:
-                write_asm_file_output("\n\t.EXPORT %s" % addr_to_label_noh(address))
-                write_asm_file_output("%s:" % addr_to_label_noh(address))
+    # get the labels for this section
+    sorted_bss_labels = list(filter(lambda x: x >= address and x < address + size, list(sorted(labelled_addresses))))
+    
+    # if there's a label right on the start of the section, properly label it
+    # if not then make sure there's proper amount of padding before the first label
+    index = -1
+    if address in sorted_bss_labels:
+        index = 0
+
+    while index < len(sorted_bss_labels):
+        if index == -1:
+            write_asm_file_output(f"\t.res.b {sorted_bss_labels[0] - address}")
+        else:
+            address = sorted_bss_labels[index]
+
+            # handle "last label" case
+            if index < len(sorted_bss_labels) - 1:
+                next_address = sorted_bss_labels[index + 1]    
             else:
-                write_asm_file_output("\n\t.EXPORT %s" % ("_lbl_%08X" % address))
-                write_asm_file_output("_lbl_%08X:" % address)
-        write_asm_file_output("\t.res.b 1")
-        address += 1
+                next_address = address + size
+    
+            write_asm_file_export("\n\t.EXPORT %s" % addr_to_label_noh(address))
+            write_asm_file_output("%s:" % addr_to_label_noh(address))
+            write_asm_file_output(f"\t.res.b {next_address - address}")
+        index += 1
 
 def disassemble_callback(seg_start, seg_end, address, offset, insn, bytes):
     raw = struct.unpack("H", filecontent[offset : offset + 2])[0]
@@ -280,19 +299,22 @@ def disassemble_callback(seg_start, seg_end, address, offset, insn, bytes):
     
     if check_invalid_data_address(address):
         return
-    
+
+    if len(insn.operands) > 0 and insn.operands[0][0] == sh4.OPER_TYPE.ADDRESS:
+        addr = insn.operands[0][1]
+        if addr in labelled_addresses and (addr < seg_start or addr >= seg_end):
+            write_asm_file_import("\t.IMPORT %s" % (addr_to_label_noh(addr)))    
+
     # todo: figure out how this case occurs, i probably put it here for a good reason
     if address in data_short_addresses and address in data_long_addresses:
         data_short_addresses.remove(address)
 
     # Output label (if any)
-    if address in labelled_addresses:
-        if address in label_names:
-            write_asm_file_export("\t.EXPORT %s" % label_names[address])
-            write_asm_file_output("%s:" % label_names[address])
-        else:
-            write_asm_file_export("\t.EXPORT %s" % ("_lbl_%08X" % address))
-            write_asm_file_output("_lbl_%08X:" % address)
+    if address in labelled_addresses:        
+        if address in need_export or address in label_names:
+            write_asm_file_export("\t.EXPORT %s" % addr_to_label_noh(address))
+        write_asm_file_output("%s:" % addr_to_label_noh(address))
+
         if address in data_short_addresses:
             write_asm_file_output("\t.data.w h'%04x" % (raw))
             return
@@ -331,6 +353,7 @@ def blacklist_label_callback(seg_start, seg_end, address, offset, insn, bytes):
 
 max_end = 0
 max_data_end = 0
+late_data = False
 is_short_data = False
 is_data = set()
 import_data = set()
@@ -338,8 +361,6 @@ visited = set()
 functions_done = set()
 
 def find_func_end(address, seg_end):
-#    print("%x" % (address), visited)
-   # print([("%x") % (x) for x in sorted(functions_done)])
     if address > seg_end:
         return
     if address in visited:
@@ -386,8 +407,10 @@ def func_search(addr, seg_end):
     global max_data_end
     global visited
     global is_data
+    global late_data
 
     # init
+    late_data = False
     max_end = 0
     max_data_end = 0
     visited = set()
@@ -421,18 +444,18 @@ def func_search(addr, seg_end):
 
     # if its not a nop, neither a data from our func 
     # (it can be directly data without nops before it, if the alignment lines up perfectly)
-    maybe_nop = get_instruction_at(max_end + 2 + key, max_end + 2)
-    if maybe_nop.op != sh4.OP.NOP and max_end + 2 not in is_data:
+    if not is_nop_padding(max_end + 2 + key, max_end + 2) and max_end + 2 not in is_data:
+        late_data = True
         return max_end + 2
 
     # in this case, after our function's code our data is there
     # we jump over the last data we found
     # and then skip past the nops
     data_section = max_data_end + (2 if is_short_data else 4)
-    maybe_nop = get_instruction_at(data_section + key, data_section)
-    while(maybe_nop.op == sh4.OP.NOP):
+    maybe_nop = is_nop_padding(data_section + key, data_section)
+    while(maybe_nop):
         data_section = data_section + 2
-        maybe_nop = get_instruction_at(data_section + key, data_section)
+        maybe_nop = is_nop_padding(data_section + key, data_section)
 
     if get_short_at(data_section) == 0 and (seg_end - data_section) < 32:
         while get_short_at(data_section) == 0:
@@ -451,16 +474,21 @@ def get_label_callback(seg_start, seg_end, address, offset, insn, bytes):
     # if branch instruction
     if insn.op in {sh4.OP.BF, sh4.OP.BRA, sh4.OP.BRAF, sh4.OP.BSR, sh4.OP.BSRF, sh4.OP.BT}:
         for op in insn.operands:
-            if op[0] == sh4.OPER_TYPE.ADDRESS:                
+            if op[0] == sh4.OPER_TYPE.ADDRESS:
+                if op[1] < seg_start or op[1] > seg_end:
+                    need_export.add(op[1])               
+
                 add_label(op[1], None)
+
     if insn.op == sh4.OP.MOV or insn.op == sh4.OP.MOVA:
         if insn.operands[0][0] == sh4.OPER_TYPE.ADDRESS:
-            if insn.operands[0][1] == 0x0C51DEA4:
-                print("!!!! %X" % address)
+            if insn.operands[0][1] < seg_start or insn.operands[0][1] > seg_end:
+                need_export.add(insn.operands[0][1])            
+
             if insn.length_suffix == sh4.SUFFIX.L or insn.op == sh4.OP.MOVA:
-                add_label_funcData(insn.operands[0][1], False)
+                add_label_funcData(seg_start, seg_end, insn.operands[0][1], False)
             else:
-                add_label_funcData(insn.operands[0][1], True)
+                add_label_funcData(seg_start, seg_end, insn.operands[0][1], True)
 
 def get_subseg_name(subseg):
     return ("code_%x" % subseg[0]) if len(subseg) < 3 else subseg[2]
@@ -491,7 +519,9 @@ for i in range(len(segs)):
         subsegs = seg['subsegments']
         for j in range(len(subsegs)):
             subseg = subsegs[j]
-            if subseg[1] != 'asm' and subseg[1] != 'libasm': 
+
+            # if the segment doesn't have code (currently, valid segments are asm, libasm, hasm)
+            if 'asm' not in subseg[1]: 
                 continue
 
             subseg_start = subseg[0]
@@ -511,7 +541,7 @@ def code_interval_step(callback):
     for interval in code_intervals:
         start = interval[0]
         end = interval[1]
-        disasm_iter(start, start + key, end - start, callback)
+        disasm_iter(start + key, end + key, start, start + key, end - start, callback)
 
 # step 0: make output "asm" directory if it doesn't exist yet
 if not os.path.exists("asm"):
@@ -528,10 +558,20 @@ labelled_addresses = { x for x in labelled_addresses if (x - 2) not in data_long
 
 # step 3: generate asm (we need to get the proper labels in step 1 and 2 so that we can output those into the asm code)
 
-def gen_asm(object_name, seg_type, start, end):
+def gen_asm(object_name, seg_type, start, end, seg_start=None, seg_end=None):
+    # we need the start/end, seg_start/end distinction for per-function
+    # disassemblies to understand not to import labels from functions in the same file
+
+    if seg_start == None:
+        seg_start = start
+
+    if seg_end == None:
+        seg_end = end
+
     # the section types that the segment types correspond to
     section_map = {
         "asm": "P",
+        "hasm": "P",
         "data": "D,DATA,ALIGN=4",
         "rodata": "C,DATA,ALIGN=4",
         "bss" : "B",
@@ -544,8 +584,8 @@ def gen_asm(object_name, seg_type, start, end):
     open_asm_file_output(object_name + ".src")
 
     write_asm_file_output("\t.SECTION %s" % section_map[seg_type])
-    if(seg_type == 'asm' or seg_type == 'libasm'):
-        disasm_iter(start, start + key, end - start, disassemble_callback)
+    if('asm' in seg_type):
+        disasm_iter(seg_start + key, seg_end + key, start, start + key, end - start, disassemble_callback)
     elif seg_type == 'bss':
         bss_iter(start, start + key, end - start)
     else:
@@ -683,11 +723,13 @@ for i in range(len(segs)):
             func = subseg_start
             while func < subseg_end:    
                 end_func = func_search(func, subseg_end)
-                #print("%x %x %x" % (func, end_func, subseg_end))
+                
                 func_va = (func + key)
                 func_name = ("_func_%08X" % func_va) if func_va not in label_names else label_names[func_va]
 
                 write_c_file("INLINE_ASM(%s, \"%s\");" % (func_name, dcsplit_config['nonmatchings_path'] + "/" + dir + subseg_name + "/" + func_name + ".src") + os.linesep)
+                #if late_data:
+                    #write_c_file("")
 
                 if end_func > subseg_end:
                     end_func = subseg_end
@@ -696,7 +738,9 @@ for i in range(len(segs)):
                     dcsplit_config['nonmatchings_path'] + "/" + dir + subseg_name + "/" + func_name, 
                     subseg[1], 
                     func, 
-                    end_func
+                    end_func,
+                    subseg_start,
+                    subseg_end
                 )       
                 func = end_func
             if c_file != None:
@@ -704,7 +748,12 @@ for i in range(len(segs)):
         continue
 
     all_intervals.append([start, end, dir + seg['name']])    
-    gen_asm(dcsplit_config['asm_path'] + "/" + dir + seg['name'], seg['type'], start, end)
+    gen_asm(
+        dcsplit_config['asm_path'] + "/" + dir + seg['name'], 
+        seg['type'], 
+        start, 
+        end
+    )
 
 def create_lnk_sub():
     lnk = open(dcsplit_config['linker_path'], "w")
@@ -738,28 +787,3 @@ def create_lnk_sub():
     lnk.close()
 
 create_lnk_sub()
-
-if writeBss == 1:
-    sectionLabels = sorted(labelled_bss_addresses)
-
-    bssAddress = sectionLabels[0]
-    bssEnd = sectionLabels[len(sectionLabels)-1]
-    j = 0
-    print("%x %x" % (bssAddress, bssEnd))
-    print("\t.SECTION B,DATA")
-    print("\t.ORG h'%08x" % bssAddress)
-    while bssAddress < bssEnd:
-        if j < len(sectionLabels):
-            gapSize = sectionLabels[j] - bssAddress
-            if gapSize != 0:
-                print("\t.res.b h'%X" % gapSize)
-            
-            print("\t.GLOBAL lbl_%08X" % sectionLabels[j])
-            print("lbl_%08X:" % sectionLabels[j])
-            j += 1
-        else:
-            gapSize = bssEnd - bssAddress
-            if gapSize != 0:
-                print("\t.res.b h'%X" % gapSize)
-        bssAddress += gapSize
-    print("\t.res.b 1")
